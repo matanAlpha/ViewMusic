04cdd0f55421f94818295c9fabe7a34226c7aad6
admin <admin@admins-imac.elcam.co.il>
2017-02-27 06:22:47 +0200

    Initial Commit

---
+++ ViewMusic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ViewMusic
+//
+//  Created by admin on 1 Adar 5777.
+//  Copyright © 5777 Matan alpha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
